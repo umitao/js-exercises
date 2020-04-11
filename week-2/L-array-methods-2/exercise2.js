270b9d9b99e1a0be85c3320bd5c02e8253789376
umitao <umitao@gmail.com>
2020-04-11 15:34:32 +0300

    exercise L complete

--- week-2/L-array-methods-2/exercise2.js
+++ week-2/L-array-methods-2/exercise2.js
@@ -7,11 +7,21 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+
+  /* ALTERNATIVE SOLUTION
+  var arr = str.split("");
+  var uppercase = arr[0].toUpperCase();
+  var final = uppercase + arr.slice(1).join("");
+  return final;
+  */
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
+
 var name = "daniel";
 
 console.log(capitalise(name));
